b968586e3ca842fc9414fb5ebe0a8e06fb72e222
= <timothywarner316@gmail.com>
2016-10-19 15:04:22 -0500

    Add value

--- azure-webapp.ps1
+++ azure-webapp.ps1
@@ -29,6 +29,10 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Force -Scope Process
 
 Update-Help -Force -ErrorAction SilentlyContinue
 
+#endregion
+
+#region Discovery
+
 Get-Command -Module AzureRM.Websites | Select-Object -Property Name | Format-Wide -Column 2
 
 Get-Command -Module Azure | Where-Object { $_.Name -like "*website*"} | Select-Object -Property Name | Format-Wide -Column 2
@@ -38,6 +42,73 @@ Get-Help -Name New-AzureRmWebApp -ShowWindow
 Get-Help -Name New-AzureWebsite -ShowWindow
 
 #endregion
+
+#region App Service Plans
+
+New-AzureRmAppServicePlan -Name ContosoAppServicePlan -Location "South Central US" -ResourceGroupName ContosoAzureResourceGroup -Tier Premium -WorkerSize Large -NumberofWorkers 10
+
+New-AzureRmAppServicePlan -Name ContosoAppServicePlan -Location "South Central US" -ResourceGroupName ContosoAzureResourceGroup -AseName constosoASE -AseResourceGroupName contosoASERG -Tier Premium -WorkerSize Large -NumberofWorkers 10
+
+Get-AzureRmAppServicePlan -ResourceGroupname ContosoAzureResourceGroup
+
+Get-AzureRmAppServicePlan -Name ContosoAppServicePlan
+
+Set-AzureRmAppServicePlan -Name ContosoAppServicePlan -ResourceGroupName ContosoAzureResourceGroup -Tier Standard -WorkerSize Medium -NumberofWorkers 9
+
+Set-AzureRmAppServicePlan -Name ContosoAppServicePlan -ResourceGroupName ContosoAzureResourceGroup -NumberofWorkers 9
+
+Set-AzureRmAppServicePlan -Name ContosoAppServicePlan -ResourceGroupName ContosoAzureResourceGroup -Tier Standard
+
+Remove-AzureRmAppServicePlan -Name ContosoAppServicePlan -ResourceGroupName ContosoAzureResourceGroup
+
+#endregion
+
+#region Create a Web App
+
+New-AzureRmWebApp -Name ContosoWebApp -AppServicePlan ContosoAppServicePlan -ResourceGroupName ContosoAzureResourceGroup -Location "South Central US"
+
+New-AzureRmWebApp -Name ContosoWebApp -AppServicePlan ContosoAppServicePlan -ResourceGroupName ContosoAzureResourceGroup -Location "South Central US"  -ASEName ContosoASEName -ASEResourceGroupName ContosoASEResourceGroupName
+
+Remove-AzureRmWebApp -Name ContosoWebApp -ResourceGroupName ContosoAzureResourceGroup
+
+Get-AzureRmWebApp
+
+Get-AzureRmWebApp -ResourceGroupname ContosoAzureResourceGroup
+
+
+
+#endregion
+
+#region Modify Web App Settings
+
+$connectionstrings = @{ ContosoConn1 = @{ Type = “MySql”; Value = “MySqlConn”}; ContosoConn2 = @{ Type = “SQLAzure”; Value = “SQLAzureConn”} }
+Set-AzureRmWebApp -Name ContosoWebApp -ResourceGroupName ContosoAzureResourceGroup -ConnectionStrings $connectionstrings
+
+$appsettings = @{appsetting1 = "appsetting1value"; appsetting2 = "appsetting2value"}
+Set-AzureRmWebApp -Name ContosoWebApp -ResourceGroupName ContosoAzureResourceGroup -AppSettings $appsettings
+
+Set-AzureRmWebApp -Name ContosoWebApp -ResourceGroupName ContosoAzureResourceGroup -Use32BitWorkerProcess $False
+
+#endregion
+
+#region Web App Run States
+
+Restart-AzureRmWebapp -Name ContosoWebApp -ResourceGroupName ContosoAzureResourceGroup
+
+Stop-AzureRmWebapp -Name ContosoWebApp -ResourceGroupName ContosoAzureResourceGroup
+
+Start-AzureRmWebapp -Name ContosoWebApp -ResourceGroupName ContosoAzureResourceGroup
+
+Get-AzureRmWebAppPublishingProfile -Name ContosoWebApp -ResourceGroupName ContosoAzureResourceGroup -OutputFile .\publishingprofile.txt
+
+#endregion
+
+#region ARM Template Deployment
+
+
+#endregion
+
+
 
 
 
