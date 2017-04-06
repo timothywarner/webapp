12d9158687841f9af111242f47f2097e94624a09
= <timothywarner316@gmail.com>
2017-04-06 09:54:10 -0500

    Add resources for app pod session

---
+++ azure-webapp-pod.ps1
@@ -0,0 +1,119 @@
+break
+# #############################################################################
+# Azure Web Apps
+# AUTHOR:  Tim Warner
+# EMAIL: timothy-warner@pluralsight.com
+# TWITTER: @TechTrainerTim
+# #############################################################################
+ 
+# Press CTRL+M to expand/collapse regions
+
+#region Obtain the Azure module
+
+Find-Module -Name AzureRM.Websites -AllVersions
+
+Install-Module -Name AzureRM -MaximumVersion -Verbose
+
+Update-Help -Force
+
+#endregion
+
+#region Connect to Azure
+
+Set-Location -Path 'C:\Users\Tim\Dropbox\path-to-repo\'
+Login-AzureRmAccount
+Get-AzureRmContext   # Get-AzureRmSubscription
+Set-AzureRmContext -SubscriptionName ''
+
+Save-AzureRmProfile -Path '.\profile.json'
+Select-AzureRmProfile -Path '.\profile.json'
+#endregion
+
+#region Discovery
+
+Get-Command -Module AzureRM.Websites | Select-Object -Property Name | Format-Wide -Column 2
+
+Get-Help New-AzureRmWebApp -ShowWindow
+
+Get-Help -Name New-AzureRmWebApp -ShowWindow
+
+Get-Help -Name New-AzureWebsite -ShowWindow
+
+#endregion
+
+#region App Service Plans
+
+Get-AzureRmAppServicePlan | Get-Member
+
+New-AzureRmResourceGroup -Name 'pssummit2017' -Location 'WestUS' 
+
+$rg = Get-AzureRmResourceGroup -Name 'pssummit2017'
+$asp = 'pssummitplan'
+
+New-AzureRmAppServicePlan -Name $asp -Location 'WestUS' -ResourceGroupName 'pssummit2017' -Tier Free -Verbose
+
+Get-AzureRmAppServicePlan -Name $asp | Format-List *
+
+Set-AzureRmAppServicePlan -Name $asp -ResourceGroupName 'pssummit2017' -Tier Standard
+
+Remove-AzureRmAppServicePlan -Name $asp -ResourceGroupName 'pssummit2017'
+
+#endregion
+
+#region Create a Web App
+
+$appName = 'pssummitwebapp101'
+
+New-AzureRmWebApp -Name $appName -AppServicePlan $asp `
+-ResourceGroupName 'pssummit2017' -Location 'WestUS'
+
+Remove-AzureRmWebApp -Name $appName -ResourceGroupName 'pssummit2017'
+
+Get-AzureRmWebApp -ResourceGroupname 'pssummit2017'
+
+#endregion
+
+#region Modify Web App Settings
+
+$connectionstrings = @{ ITEdgeConn1 = @{ Type = 'MySql'; Value = 'MySqlConn'}; ITEdgeConn2 = @{ Type = 'SQLAzure'; Value = 'SQLAzureConn'} }
+Set-AzureRmWebApp -Name $appName -ResourceGroupName 'pssummit2017' -ConnectionStrings $connectionstrings
+
+$appsettings = @{appsetting1 = "appsetting1value"; appsetting2 = "appsetting2value"}
+
+Set-AzureRmWebApp -Name $appName -ResourceGroupName 'pssummit2017' -AppSettings $appsettings
+
+Set-AzureRmWebApp -Name $appName -ResourceGroupName 'pssummit2017' -Use32BitWorkerProcess $False
+
+#endregion
+
+#region Web App Run States
+
+Restart-AzureRmWebapp -Name $appName -ResourceGroupName 'pssummit2017'
+
+Stop-AzureRmWebapp -Name $appName -ResourceGroupName 'pssummit2017'
+
+Start-AzureRmWebapp -Name $appName -ResourceGroupName 'pssummit2017'
+
+Get-AzureRmWebAppPublishingProfile -Name $appName -ResourceGroupName 'pssummit2017' -OutputFile 'c:\profile.xml'
+
+ise 'C:\profile.xml'
+
+#endregion
+
+#region ARM Template Deployment
+
+Set-Location -Path 'D:\Dropbox (Personal)\PowerShell Summit 2017\tues-Azure-Web-Apps\webapp\simplewebapp'
+
+# show in VS 2017
+ise ./azuredeploy.json
+
+ise ./azuredeploy.parameters.json
+
+New-AzureRmResourceGroupDeployment -Name 'Webapp2GitHub' -ResourceGroupName 'pssummit2017' `
+# -TemplateUri '' `
+-TemplateFile '.\azuredeploy.json' `
+-TemplateParameterFile '.\azuredeploy.parameters.json' `
+# -TemplateParameterUri '' `
+-Mode Complete
+
+#endregion
